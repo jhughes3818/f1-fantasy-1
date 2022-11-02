56f2c0a23f588ac9463bb8a94e5dca67f7174f99
Jason <jhughes3818@gmail.com>
2022-11-02 17:47:58 +1100

    add nextauth route

---
+++ pages/api/auth/[...nextauth].jsx
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  options: {
+    maxAge: 60,
+  },
+
+  secret: process.env.SECRET,
+};
+
+export default NextAuth(authOptions);
