225d6bf14378d92585a70a12fd18702bb28d8787
Jason <jhughes3818@gmail.com>
2022-11-03 19:41:19 +1100

    added confirmation modal

--- components/leagues/JoinLeague.jsx
+++ components/leagues/JoinLeague.jsx
@@ -2,9 +2,15 @@ import Link from "next/link";
 import { useSession } from "next-auth/react";
 import axios from "axios";
 import { useState } from "react";
+import Modal from "../team-build/Modal";
 
 export default function JoinLeague(props) {
+  const [isOpen, setIsOpen] = useState(false);
   const [codeEntered, setCodeEntered] = useState();
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   async function joinLeague(user, leagueCode) {
     //Validate that league code is valid
@@ -30,42 +36,54 @@ export default function JoinLeague(props) {
           console.log("Added user to league");
         }
       });
+
+    setIsOpen(true);
   }
   const { data: session } = useSession();
   return (
-    <div className="grid place-items-center">
-      <div className="text-center">
-        <h1 className="text-3xl font-bold mb-3">Join A League</h1>
+    <div>
+      <Modal
+        heading="Successfully Joined League"
+        buttonText="Got it"
+        function={closeModal}
+        isOpen={isOpen}
+      />
+      <div className="grid place-items-center">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold mb-3">Join A League</h1>
 
-        <input
-          placeholder="Enter League Code"
-          className="block mb-3 box-styling px-2 w-56 text-center"
-          autoFocus
-          onChange={(e) => setCodeEntered(e.target.value)}
-        />
-        <button
-          onClick={() => joinLeague(session.user, codeEntered)}
-          className="block box-styling bg-blue-500 text-white text-center font-bold w-56 mb-3"
-        >
-          Join
-        </button>
-        {props.showSkip ? (
-          <div>
-            <Link href="/">
-              <span className="text-gray-500 cursor-pointer">Skip for now</span>
-            </Link>
-          </div>
-        ) : null}
-        {props.showCreate ? (
-          <div>
-            <p>or</p>
-            <Link href="/">
-              <span className="text-gray-500 cursor-pointer">
-                Make your own league
-              </span>
-            </Link>
-          </div>
-        ) : null}
+          <input
+            placeholder="Enter League Code"
+            className="block mb-3 box-styling px-2 w-56 text-center"
+            autoFocus
+            onChange={(e) => setCodeEntered(e.target.value)}
+          />
+          <button
+            onClick={() => joinLeague(session.user, codeEntered)}
+            className="block box-styling bg-blue-500 text-white text-center font-bold w-56 mb-3"
+          >
+            Join
+          </button>
+          {props.showSkip ? (
+            <div>
+              <Link href="/">
+                <span className="text-gray-500 cursor-pointer">
+                  Skip for now
+                </span>
+              </Link>
+            </div>
+          ) : null}
+          {props.showCreate ? (
+            <div>
+              <p>or</p>
+              <Link href="/">
+                <span className="text-gray-500 cursor-pointer">
+                  Make your own league
+                </span>
+              </Link>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
