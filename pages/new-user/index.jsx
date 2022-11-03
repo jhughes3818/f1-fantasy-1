acbf0c2f7daba451fa23b1ba8e8176b3630e3814
Jason <jhughes3818@gmail.com>
2022-11-03 14:22:21 +1100

    welcome screen

--- pages/new-user/index.jsx
+++ pages/new-user/index.jsx
@@ -1,7 +1,30 @@
+import Link from "next/link";
+
 export default function Welcome() {
   return (
     <div className="grid place-items-center h-screen">
-      <h1>Hello</h1>
+      <div className="w-80">
+        <h1 className="text-2xl">Hello 👋</h1>
+        <h2 className="pb-4">Let's go racing.</h2>
+        <p className="text-gray-600">
+          We’ll start by building a team.
+          <br />
+          <br />
+          You have $30m to spend on drivers.
+          <br />
+          <br />
+          Each race, your drivers will earn points. The price of each driver
+          will also go up or down based on their performance.
+          <br />
+          <br />
+          You can build your budget by selling drivers for a profit.
+        </p>
+        <Link href="/">
+          <div className="box-styling bg-blue-500 text-white font-bold text-center mt-4 cursor-pointer">
+            <span>Next</span>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
