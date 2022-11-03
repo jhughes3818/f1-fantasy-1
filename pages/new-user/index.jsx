a064346794ab530af49708c0d51d0d74ce66d413
Jason <jhughes3818@gmail.com>
2022-11-03 14:30:20 +1100

    escaped character

--- pages/new-user/index.jsx
+++ pages/new-user/index.jsx
@@ -5,7 +5,7 @@ export default function Welcome() {
     <div className="grid place-items-center h-screen">
       <div className="w-80">
         <h1 className="text-2xl">Hello 👋</h1>
-        <h2 className="pb-4">Let's go racing.</h2>
+        <h2 className="pb-4">Let&apos;s go racing.</h2>
         <p className="text-gray-600">
           We&apos;ll start by building a team.
           <br />
