8734bcd66b8873d6c7c83acbfafc798663d05a39
Jason <jhughes3818@gmail.com>
2022-11-04 16:54:17 +1100

    removed double useremail in get request

--- pages/api/users/[userEmail].jsx
+++ pages/api/users/[userEmail].jsx
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     res.status(200).json({ message: "Successfully joined league" });
   }
   if (req.method === "GET") {
-    const user = await User.findOne({ email: userEmail.userEmail }).exec();
+    const user = await User.findOne({ email: userEmail }).exec();
     if (user) {
       res.status(200).json({ user: user });
     } else {
