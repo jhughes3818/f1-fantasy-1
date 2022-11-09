087119cf3cd429f98ff8a181e685bf1573f9eca3
Jason <jhughes3818@gmail.com>
2022-11-09 19:07:08 +1100

    removed login from league page

--- pages/league.jsx
+++ pages/league.jsx
@@ -70,14 +70,18 @@ export default function EditTeam() {
   } else {
     return (
       <div className="grid place-items-center h-screen">
-        <div className="box-styling h-36 text-center px-4">
-          <h1 className="text-2xl font-bold text-center">
-            Hi! Please sign in to continue.
-          </h1>
-          <button className="box-styling mt-6 p-3" onClick={() => signIn()}>
-            Sign in
-          </button>
-        </div>
+        <Oval
+          height={80}
+          width={80}
+          color="#000000"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="oval-loading"
+          secondaryColor="#2a2b2a"
+          strokeWidth={2}
+          strokeWidthSecondary={2}
+        />
       </div>
     );
   }
