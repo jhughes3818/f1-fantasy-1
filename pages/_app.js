70eb7e4809a637614a0e6e7ad2c039c0190ca986
Jason <jhughes3818@gmail.com>
2022-11-09 19:58:28 +1100

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,20 @@
 import "../styles/globals.css";
 
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Fantasy Grid - Coming Soon</title>
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
