c5a3cc9d5961e98233212da0ef036a10f98f4682
Jason <jhughes3818@gmail.com>
2022-11-11 15:58:08 +1100

    added multi-select basics

--- components/team-build/MultiSelect.jsx
+++ components/team-build/MultiSelect.jsx
@@ -1,159 +1,71 @@
-import React, { useState } from "react";
+import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
 const people = [
-  "Wade Cooper",
-  "Arlene Mccoy",
-  "Devon Webb",
-  "Tom Cook",
-  "Tanya Fox",
-  "Hellen Schmidt",
-  "Caroline Schultz",
-  "Mason Heaney",
-  "Claudie Smitham",
-  "Emil Schaefer",
+  { name: "Wade Cooper" },
+  { name: "Arlene Mccoy" },
+  { name: "Devon Webb" },
+  { name: "Tom Cook" },
+  { name: "Tanya Fox" },
+  { name: "Hellen Schmidt" },
 ];
 
 export default function MultiSelect() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedPersons, setSelectedPersons] = useState([]);
-
-  function isSelected(value) {
-    return selectedPersons.find((el) => el === value) ? true : false;
-  }
-
-  function handleSelect(value) {
-    if (!isSelected(value)) {
-      const selectedPersonsUpdated = [
-        ...selectedPersons,
-        people.find((el) => el === value),
-      ];
-      setSelectedPersons(selectedPersonsUpdated);
-    } else {
-      handleDeselect(value);
-    }
-    setIsOpen(true);
-  }
-
-  function handleDeselect(value) {
-    const selectedPersonsUpdated = selectedPersons.filter((el) => el !== value);
-    setSelectedPersons(selectedPersonsUpdated);
-    setIsOpen(true);
-  }
+  const [selected, setSelected] = useState(people[0]);
 
   return (
-    <div className="flex items-center justify-center p-12">
-      <div className="w-full max-w-xs mx-auto">
-        <Listbox
-          as="div"
-          className="space-y-1"
-          value={selectedPersons}
-          onChange={(value) => handleSelect(value)}
-          open={isOpen}
-        >
-          {() => (
-            <>
-              <Listbox.Label className="block text-sm leading-5 font-medium text-gray-700">
-                Assigned to
-              </Listbox.Label>
-              <div className="relative">
-                <span className="inline-block w-full rounded-md shadow-sm">
-                  <Listbox.Button
-                    className="cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-                    onClick={() => setIsOpen(!isOpen)}
-                    open={isOpen}
-                  >
-                    <span className="block truncate">
-                      {selectedPersons.length < 1
-                        ? "Select persons"
-                        : `Selected persons (${selectedPersons.length})`}
-                    </span>
-                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                      <svg
-                        className="h-5 w-5 text-gray-400"
-                        viewBox="0 0 20 20"
-                        fill="none"
-                        stroke="currentColor"
-                      >
-                        <path
-                          d="M7 7l3-3 3 3m0 6l-3 3-3-3"
-                          strokeWidth="1.5"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                    </span>
-                  </Listbox.Button>
-                </span>
-
-                <Transition
-                  unmount={false}
-                  show={isOpen}
-                  leave="transition ease-in duration-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                  className="absolute mt-1 w-full rounded-md bg-white shadow-lg"
+    <div className="fixed top-16 w-72">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <span className="block truncate">{selected.name}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {people.map((person, personIdx) => (
+                <Listbox.Option
+                  key={personIdx}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? "bg-amber-100 text-amber-900" : "text-gray-900"
+                    }`
+                  }
+                  value={person}
                 >
-                  <Listbox.Options
-                    static
-                    className="max-h-60 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5"
-                  >
-                    {people.map((person) => {
-                      const selected = isSelected(person);
-                      return (
-                        <Listbox.Option key={person} value={person}>
-                          {({ active }) => (
-                            <div
-                              className={`${
-                                active
-                                  ? "text-white bg-blue-600"
-                                  : "text-gray-900"
-                              } cursor-default select-none relative py-2 pl-8 pr-4`}
-                            >
-                              <span
-                                className={`${
-                                  selected ? "font-semibold" : "font-normal"
-                                } block truncate`}
-                              >
-                                {person}
-                              </span>
-                              {selected && (
-                                <span
-                                  className={`${
-                                    active ? "text-white" : "text-blue-600"
-                                  } absolute inset-y-0 left-0 flex items-center pl-1.5`}
-                                >
-                                  <svg
-                                    className="h-5 w-5"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                  >
-                                    <path
-                                      fillRule="evenodd"
-                                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                      clipRule="evenodd"
-                                    />
-                                  </svg>
-                                </span>
-                              )}
-                            </div>
-                          )}
-                        </Listbox.Option>
-                      );
-                    })}
-                  </Listbox.Options>
-                </Transition>
-                <div className="pt-1 text-sm">
-                  {selectedPersons.length > 0 && (
-                    <>Selected persons: {selectedPersons.join(", ")}</>
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {person.name}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
                   )}
-                </div>
-              </div>
-            </>
-          )}
-        </Listbox>
-      </div>
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
     </div>
   );
 }
