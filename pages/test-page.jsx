5cb951fbe135b5365c24d90ca8c2c29314b3b028
Jason <jhughes3818@gmail.com>
2022-11-11 16:38:37 +1100

    added listbox elements

--- pages/test-page.jsx
+++ pages/test-page.jsx
@@ -1,10 +1,11 @@
 import ProgressBars from "../components/misc/ProgressBars";
+import DriverTrade from "../components/team-build/DriverTrade";
 import MultiSelect from "../components/team-build/MultiSelect.jsx";
 
 export default function Test() {
   return (
     <div className="grid place-items-center">
-      <MultiSelect />
+      <DriverTrade />
     </div>
   );
 }
