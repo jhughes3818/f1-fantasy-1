f661f64581f9a1a31d7d483fa43235205aead956
Jason <jhughes3818@gmail.com>
2022-11-12 15:48:07 +1100

    styled trade screen

--- components/team-build/DriverTrade.jsx
+++ components/team-build/DriverTrade.jsx
@@ -91,13 +91,15 @@ export default function DriverTrade() {
 
   if (selected1 != null && selected2 != null) {
     return (
-      <>
+      <div className="grid place-items-center h-screen">
         <div className="gap-4">
-          <div className="w-80">
-            <h1>Select Driver To Sell</h1>
+          <div className="w-80 my-4">
+            <h1 className="font-bold text-lg w-full text-center">
+              Select Driver To Sell
+            </h1>
             <Listbox value={selected1} onChange={setSelected1}>
               <div className="relative mt-1 z-40">
-                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm h-10">
                   <span className="block truncate">
                     {selected1.name}{" "}
                     <span className="text-gray-500">({selected1.price}m)</span>
@@ -158,10 +160,12 @@ export default function DriverTrade() {
             </Listbox>
           </div>
           <div className="w-80">
-            <h1>Select Driver to Buy</h1>
+            <h1 className="font-bold text-lg w-full text-center">
+              Select Driver to Buy
+            </h1>
             <Listbox value={selected2} onChange={setSelected2}>
               <div className="relative mt-1 z-30">
-                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm h-10">
                   <span className="block truncate">
                     {selected2.name}{" "}
                     <span className="text-gray-500">({selected2.price}m)</span>
@@ -221,29 +225,35 @@ export default function DriverTrade() {
               </div>
             </Listbox>
           </div>
-        </div>
-        <div className="grid place-items-center my-4 w-80">
-          {profit > 0 ? (
-            <h1 className="text-xl font-bold text-green-600">
-              Profit: {profit}
-            </h1>
-          ) : (
-            <h1 className="text-xl font-bold text-red-600">Profit: {profit}</h1>
-          )}
 
-          <form className="grid place-items-center gap-3">
-            <input
-              placeholder="Reason for trade"
-              type="text"
-              className="block mb-3 box-styling px-2 w-56 text-center"
-              onChange={(e) => setMessage(e.target.value)}
-            ></input>
-          </form>
-          <button onClick={() => confirmTrade()} className="button-styling">
-            Confirm Trade
-          </button>
+          <div className="grid place-items-center my-4 w-80">
+            {profit > 0 ? (
+              <h1 className="text-xl font-bold text-green-600 mb-4">
+                Profit: ${profit}m
+              </h1>
+            ) : (
+              <h1 className="text-xl font-bold text-red-600 mb-4">
+                Profit: ${profit}m
+              </h1>
+            )}
+
+            <form className="grid place-items-center gap-3">
+              <input
+                placeholder="Reason for trade"
+                type="text"
+                className="block mb-3 box-styling px-2 w-80 text-center h-10"
+                onChange={(e) => setMessage(e.target.value)}
+              ></input>
+            </form>
+            <button
+              onClick={() => confirmTrade()}
+              className="button-styling h-10 w-80"
+            >
+              Confirm Trade
+            </button>
+          </div>
         </div>
-      </>
+      </div>
     );
   } else {
     return <h1>Loading...</h1>;
