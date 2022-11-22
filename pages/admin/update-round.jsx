c5f3d92b5a70e7019960c0451bd6113bc9db4577
Jason <jhughes3818@gmail.com>
2022-11-22 15:28:12 +1100

    got admin page working

--- pages/admin/update-round.jsx
+++ pages/admin/update-round.jsx
@@ -1,17 +1,38 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default function UpdateRound() {
   const [round, setRound] = useState();
+  const [done, setDone] = useState();
+  const [roundDone, setRoundDone] = useState();
+
+  async function handleClick(nextRound) {
+    axios.put("/api/admin/update-round", { round: round }).then((response) => {
+      if (response.status === 200) {
+        setDone(true);
+        setRoundDone(response.data.round);
+        console.log(response.data.round);
+      }
+    });
+  }
+
   return (
     <>
+      {done ? <h1>Done!</h1> : <h1>Not Done Yet!</h1>}
       <div className="grid place-items-center">
         <h1>{round}</h1>
+        <h1>{roundDone}</h1>
         <input
           className="input-styling"
           placeholder="Latest Round"
           onChange={(e) => setRound(e.target.value)}
         ></input>
-        <button className="button-styling">Update</button>
+        <button onClick={() => handleClick(round)} className="button-styling">
+          Get Latest Data
+        </button>
+        <button onClick={() => handleClick(round)} className="button-styling">
+          Update League Points
+        </button>
       </div>
     </>
   );
--- pages/api/admin/update-round.jsx
+++ pages/api/admin/update-round.jsx
@@ -1,11 +1,68 @@
-import GetLatestRoundData from "../../../functions/GetLatestRoundData";
-import UpdateTeamPoints from "../../../functions/UpdateTeamPoints";
+import { Race, User } from "../../../database/schemas";
+import mongoose from "mongoose";
+import axios from "axios";
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri);
+
+const GetLatestRoundData = (round) => {
+  axios
+    .get(`http://ergast.com/api/f1/current/${round}/results.json`)
+    .then((response) => {
+      const raceResults = response.data.MRData.RaceTable.Races[0].Results;
+      raceResults.forEach((driver) => {
+        const results = {
+          finishingPosition: driver.position,
+          startingPosition: driver.grid,
+          points: driver.points,
+          positionsGained: driver.grid - driver.position,
+        };
+
+        const name = driver.Driver.givenName + " " + driver.Driver.familyName;
+
+        Race.findOneAndUpdate(
+          { name: name },
+          {
+            $push: { results: results },
+          }
+        ).exec();
+      });
+    });
+};
+
+async function UpdateTeamPoints(round) {
+  const driverResults = await Race.find().exec();
+  const driverObject = {};
+  driverResults.forEach((driver) => {
+    driverObject[driver.name] = driver.results[1];
+  });
+
+  console.log(driverObject);
+  const users = await User.find().exec();
+
+  users.forEach((user) => {
+    let pointsScoredTeam = user.points;
+    const team = user.team;
+    team.forEach((driver) => {
+      const name = driver.name;
+      const pointsScored = driverObject[name];
+
+      pointsScoredTeam = pointsScoredTeam + parseInt(pointsScored.points);
+    });
+    User.findOneAndUpdate(
+      { email: user.email },
+      { points: pointsScoredTeam }
+    ).exec();
+  });
+}
 
 export default async function handler(req, res) {
-  //await GetLatestRoundData();
+  const roundToGet = req.body.round;
+  await GetLatestRoundData(roundToGet);
   console.log("Got round data");
 
   await UpdateTeamPoints();
   console.log("Updated Points");
-  res.status(200).json({ message: "done" });
+  console.log(roundToGet);
+  res.status(200).json({ message: "done", round: roundToGet });
 }
