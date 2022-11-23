b0cd5d9cf2b889a4406f4ee4f6145e1db299b706
Jason <jhughes3818@gmail.com>
2022-11-23 17:38:21 +1100

    add steps in onboarding

--- pages/new-user/create-team.jsx
+++ pages/new-user/create-team.jsx
@@ -1,13 +1,20 @@
 import TeamBuildMobile from "../../components/TeamBuildMobile";
 import { useState, useEffect } from "react";
 import ProgressBars from "../../components/misc/ProgressBars";
+import Steps from "../../components/misc/Steps";
+
+const steps = [
+  { id: "Step 1", name: "Welcome", href: "#", status: "complete" },
+  { id: "Step 2", name: "Build Team", href: "#", status: "current" },
+  { id: "Step 3", name: "Join League", href: "#", status: "upcoming" },
+];
 
 export default function CreateTeam() {
   return (
     <div className="grid place-items-center h-screen">
-      <div className="grid place-items-center">
-        <div className="mb-2">
-          <ProgressBars bars={1} />
+      <div className="">
+        <div className="mb-6">
+          <Steps steps={steps} />
         </div>
         <h1 className="text-3xl mb-6 text-center font-bold">
           Create Your Team
--- pages/new-user/join-league.jsx
+++ pages/new-user/join-league.jsx
@@ -1,13 +1,20 @@
 import Link from "next/link";
 import JoinLeague from "../../components/leagues/JoinLeague";
 import ProgressBars from "../../components/misc/ProgressBars";
+import Steps from "../../components/misc/Steps";
+
+const steps = [
+  { id: "Step 1", name: "Welcome", href: "#", status: "complete" },
+  { id: "Step 2", name: "Build Team", href: "#", status: "complete" },
+  { id: "Step 3", name: "Join League", href: "#", status: "current" },
+];
 
 export default function JoinLeaguePage(props) {
   return (
     <div className="grid place-items-center h-screen">
       <div>
-        <div className="mb-4">
-          <ProgressBars bars={2} />
+        <div className="mb-8">
+          <Steps steps={steps} />
         </div>
         <JoinLeague showSkip={true} showCreate={true} />
       </div>
