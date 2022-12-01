9932f417d138e1ee09a9af152942ed27219a5db3
Jason <jhughes3818@gmail.com>
2022-12-01 20:17:12 +1100

    added supabase auth

--- pages/_app.js
+++ pages/_app.js
@@ -2,22 +2,28 @@ import "../styles/globals.css";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
-
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 const queryClient = new QueryClient();
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
   return (
     <>
       <Head>
         <title>Fantasy Grid</title>
       </Head>
       <QueryClientProvider client={queryClient}>
-        <SessionProvider session={session}>
+        <SessionContextProvider
+          supabaseClient={supabase}
+          initialSession={pageProps.initialSession}
+        >
           <Component {...pageProps} />
-        </SessionProvider>
+        </SessionContextProvider>
       </QueryClientProvider>
     </>
   );
---
+++ pages/new-home.jsx
@@ -0,0 +1,26 @@
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import Account from "../components/supabase-auth/Account";
+
+const Home = () => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
+  return (
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          theme="dark"
+        />
+      ) : (
+        <div className="grid place-items-center">
+          <Account session={session} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
