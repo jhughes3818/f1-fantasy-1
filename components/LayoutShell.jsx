9159593c2ecd50d87e667e669b4a6229947ebbd6
Jason <jhughes3818@gmail.com>
2022-12-02 17:29:40 +1100

    added supabase to team build mobile

--- components/LayoutShell.jsx
+++ components/LayoutShell.jsx
@@ -108,16 +108,9 @@ export default function LayoutShell(props) {
                   <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
                     <a href="/profile" className="group block flex-shrink-0">
                       <div className="flex items-center">
-                        <div>
-                          <img
-                            className="inline-block h-10 w-10 rounded-full"
-                            src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-                            alt=""
-                          />
-                        </div>
                         <div className="ml-3">
                           <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
-                            Whitney Francis
+                            {session.user.email}
                           </p>
                           <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
                             View profile
@@ -179,18 +172,9 @@ export default function LayoutShell(props) {
               <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
                 <a href="/profile" className="group block w-full flex-shrink-0">
                   <div className="flex items-center">
-                    <div>
-                      {session ? (
-                        <img
-                          className="inline-block h-9 w-9 rounded-full"
-                          src={session.user.image}
-                          alt=""
-                        />
-                      ) : null}
-                    </div>
                     <div className="ml-3">
                       <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
-                        {session ? session.user.name : "..."}
+                        {session ? session.user.email : "..."}
                       </p>
                       <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">
                         View profile
--- pages/test-page.jsx
+++ pages/test-page.jsx
@@ -1,11 +1,14 @@
 import Layout from "../components/Layout";
 import NewTeamBuildMobile from "../components/team-build/NewTeamBuildMobile";
 import SupaBaseTest from "../components/test-components/supabaseTest";
+import Team from "../components/supabase-auth/Team";
+import { useSession } from "@supabase/auth-helpers-react";
 
 export default function Test() {
+  const session = useSession();
   return (
     <div className="grid place-items-center h-screen">
-      <SupaBaseTest />
+      {session ? <NewTeamBuildMobile /> : null}
     </div>
   );
 }
