7aa7438796281936e8ff618512595a4d776730de
Jason <jhughes3818@gmail.com>
2022-12-26 10:37:18 +0800

    add styling for table

--- components/leagues/LeagueTable.jsx
+++ components/leagues/LeagueTable.jsx
@@ -61,14 +61,14 @@ export default function LeagueTable(props) {
         <div className="sm:flex-auto">
           <h1 className="text-xl font-semibold text-gray-900">Your League</h1>
         </div>
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+        {/* <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
           <button
             type="button"
             className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
           >
             Edit Team
           </button>
-        </div>
+        </div> */}
       </div>
       <div className="-mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
         <table className="min-w-full divide-y divide-gray-300">
@@ -92,9 +92,10 @@ export default function LeagueTable(props) {
               >
                 Value
               </th>
-              <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                <span className="sr-only">Edit</span>
-              </th>
+              <th
+                scope="col"
+                className="px-3 py-3.5 text-right text-sm font-semibold text-gray-900"
+              ></th>
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-200 bg-white">
@@ -117,7 +118,12 @@ export default function LeagueTable(props) {
                   {person.points}
                 </td>
                 <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                  {person.cash}
+                  ${person.cash}m
+                </td>
+                <td className="hidden px-3 py-4 text-right text-sm text-gray-500 sm:table-cell">
+                  <a className="text-indigo-600 hover:text-indigo-700 cursor-pointer font-bold">
+                    View Team
+                  </a>
                 </td>
               </tr>
             ))}
