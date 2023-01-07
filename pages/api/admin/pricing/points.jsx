7df994afbc83cec838bda463ae0f85692435a659
Jason <jhughes3818@gmail.com>
2023-01-07 13:41:13 +0800

    added points algorithm

---
+++ pages/api/admin/pricing/points.jsx
@@ -0,0 +1,46 @@
+import supabase from "../../../../database/supabaseClient";
+
+export default async function handler(req, res) {
+  const drivers = await supabase.from("drivers").select("*");
+
+  const overtakesPoints = 1;
+  const qualifyingPoints = 2;
+  const finishingPoints = 3;
+
+  // For each driver, get the average qualifying position, average finishing position and average overtakes. Then calculate the points for each of these and save to an array with the driver_id as the key.
+
+  const driverPoints = [];
+  drivers.data.forEach((driver) => {
+    const averageQualifyingPosition = driver.average_qualifying_position;
+    const averageFinishingPosition = driver.average_finishing_position;
+    const averageOvertakes = driver.average_overtakes;
+
+    const qualifyingPoints = averageQualifyingPosition * 2;
+    const finishingPoints = averageFinishingPosition * 3;
+    const overtakesPoints = averageOvertakes * 1;
+
+    driverPoints.push({
+      driver: driver.ergast_id,
+      qualifying: qualifyingPoints,
+      finishing: finishingPoints,
+      overtakes: overtakesPoints,
+    });
+  });
+
+  console.log(driverPoints);
+
+  // Update points column in driver table with the points for each driver.
+  for (let i = 0; i < driverPoints.length; i++) {
+    const { data, error } = await supabase
+      .from("drivers")
+      .update({
+        points:
+          driverPoints[i].qualifying +
+          driverPoints[i].finishing +
+          driverPoints[i].overtakes,
+      })
+      .eq("ergast_id", driverPoints[i].driver);
+  }
+
+  res.status(200).json({ message: "success" });
+}
