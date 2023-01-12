249c6365e620a894e1f181b1a670db722b80025e
Jason <jhughes3818@gmail.com>
2023-01-12 17:39:44 +0800

    add new join league endpoint

--- pages/api/leagues/join/[leagueCode].jsx
+++ pages/api/leagues/join/[leagueCode].jsx
@@ -1,51 +1,23 @@
-import mongoose from "mongoose";
-import { League, User } from "../../../../database/schemas";
-
 export default async function handler(req, res) {
-  const uri = process.env.MONGODB_URI;
-  mongoose.connect(uri);
-
-  const { leagueCode } = req.query;
-
-  if (req.method === "GET") {
-    const league = await League.findOne({ code: leagueCode }).exec();
-
-    if (!league) {
-      res.status(404).json({ error: "Cannot find league" });
-    } else {
-      res.status(200).json({ league: league });
-    }
-  } else if (req.method === "PUT") {
-    const newMember = {
-      name: req.body.user.name,
-      email: req.body.user.email,
-      points: 0,
+  // Update user's league
+  if (req.method === "PUT") {
+    const updates = {
+      league: req.query.leagueCode,
     };
-    //const newMember = req.body.user;
-    const league = await League.findOne({ code: leagueCode }).exec();
-    const members = league.members;
-    let memberEmails = [];
-    members.forEach((member) => {
-      memberEmails.push(member.email);
-    });
-    console.log(memberEmails);
-    if (memberEmails.includes(req.body.user.email) === false) {
-      members.push(newMember);
 
-      await League.findOneAndUpdate(
-        { code: leagueCode },
-        { members: members }
-      ).exec();
+    // UserID is passed in in the body of the request
+    const userID = req.body.user.id;
+
+    const { error } = await supabase
+      .from("profiles")
+      .upsert(updates, { returning: "minimal" })
+      .eq("id", userID);
+
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: error });
+    } else {
+      res.status(200).json({ message: "User updated" });
     }
-
-    console.log("request received");
-    console.log(leagueCode);
-    await User.findOneAndUpdate(
-      { email: req.body.user.email },
-      { league: leagueCode }
-      //req.body
-    ).exec();
-
-    res.status(200).json({ message: "Added user to league" });
   }
 }
