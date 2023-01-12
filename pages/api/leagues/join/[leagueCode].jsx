7e3155080b2a833dff2e00830aca8deaa6a94f62
Jason <jhughes3818@gmail.com>
2023-01-12 17:43:04 +0800

    changed import of supabase

--- pages/api/leagues/join/[leagueCode].jsx
+++ pages/api/leagues/join/[leagueCode].jsx
@@ -1,4 +1,4 @@
-import { supabase } from "../../../../utils/supabaseClient";
+import { supabase } from "../../../../database/supabaseClient";
 
 export default async function handler(req, res) {
   // Update user's league
