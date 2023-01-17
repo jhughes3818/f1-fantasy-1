ec96d5a7ad2d9fd5e1ba0e05736c8ed8111a589b
Jason <jhughes3818@gmail.com>
2023-01-17 16:46:01 +0800

    change dismiss button

--- components/feed/NewFeedComponent.jsx
+++ components/feed/NewFeedComponent.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { CheckIcon } from "@heroicons/react/24/outline";
+import { CheckIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -182,6 +182,15 @@ export default function NewFeedComponent() {
                         leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                       >
                         <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                          <div className="absolute right-3 top-3">
+                            <button
+                              type="button"
+                              className="inline-flex justify-center  h-5 w-5"
+                              onClick={() => setOpen(false)}
+                            >
+                              <XMarkIcon />
+                            </button>
+                          </div>
                           <div>
                             {/* <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
                               <CheckIcon
@@ -204,15 +213,6 @@ export default function NewFeedComponent() {
                               </div>
                             </div> */}
                             <DriverTrade />
-                          </div>
-                          <div className="mt-5 sm:mt-6">
-                            <button
-                              type="button"
-                              className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
-                              onClick={() => setOpen(false)}
-                            >
-                              Go back to dashboard
-                            </button>
                           </div>
                         </Dialog.Panel>
                       </Transition.Child>
