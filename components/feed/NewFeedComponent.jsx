ed47bf87a4ba7aab1f459517ec8a34e4c10bf35d
Jason <jhughes3818@gmail.com>
2023-01-17 16:29:40 +0800

    add driver trade to modal

--- components/feed/NewFeedComponent.jsx
+++ components/feed/NewFeedComponent.jsx
@@ -1,17 +1,19 @@
 import { Fragment } from "react";
-import { ChatBubbleLeftEllipsisIcon, TagIcon } from "@heroicons/react/20/solid";
-
+import { Dialog, Transition } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/24/outline";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import FeedEntries from "./FeedEntries";
 import JoinLeague from "../leagues/JoinLeague";
+import DriverTrade from "../team-build/DriverTrade";
 
 export default function NewFeedComponent() {
   const session = useSession();
   const [activity, setActivity] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [noLeague, setNoLeague] = useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (session) {
@@ -149,10 +151,75 @@ export default function NewFeedComponent() {
 
               <button
                 type="button"
-                className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-6"
+                className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-6 xl:hidden"
+                onClick={() => setOpen(true)}
               >
                 Trade
               </button>
+              <Transition.Root show={open} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={setOpen}>
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                  </Transition.Child>
+
+                  <div className="fixed inset-0 z-10 overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                      <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enterTo="opacity-100 translate-y-0 sm:scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                      >
+                        <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                          <div>
+                            {/* <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                              <CheckIcon
+                                className="h-6 w-6 text-green-600"
+                                aria-hidden="true"
+                              />
+                            </div>
+                            <div className="mt-3 text-center sm:mt-5">
+                              <Dialog.Title
+                                as="h3"
+                                className="text-lg font-medium leading-6 text-gray-900"
+                              >
+                                Payment successful
+                              </Dialog.Title>
+                              <div className="mt-2">
+                                <p className="text-sm text-gray-500">
+                                  Lorem ipsum dolor sit amet consectetur
+                                  adipisicing elit. Consequatur amet labore.
+                                </p>
+                              </div>
+                            </div> */}
+                            <DriverTrade />
+                          </div>
+                          <div className="mt-5 sm:mt-6">
+                            <button
+                              type="button"
+                              className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+                              onClick={() => setOpen(false)}
+                            >
+                              Go back to dashboard
+                            </button>
+                          </div>
+                        </Dialog.Panel>
+                      </Transition.Child>
+                    </div>
+                  </div>
+                </Dialog>
+              </Transition.Root>
             </>
           ) : (
             <h1>Loading...</h1>
