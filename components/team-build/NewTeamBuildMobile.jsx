9f9bd49118d82c6ba6fb78d14815cc587f5bb73f
Jason <jhughes3818@gmail.com>
2023-01-19 19:26:54 +0800

    increase initial cash

--- components/team-build/NewTeamBuildMobile.jsx
+++ components/team-build/NewTeamBuildMobile.jsx
@@ -14,7 +14,7 @@ export default function NewTeamBuildMobile(props) {
 
   //States used for progress of building team
   const [driversCount, setDriversCount] = useState(0);
-  const [cash, setCash] = useState(50000);
+  const [cash, setCash] = useState(100000);
 
   const [drivers, setDrivers] = useState([]);
   const [driversNames, setDriversNames] = useState([]);
