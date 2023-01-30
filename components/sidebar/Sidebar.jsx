7da02e6437fe83a8b7ed80209dd693f7a3639bb6
Jason <jhughes3818@gmail.com>
2023-01-31 08:12:18 +1100

    styling changed to sidebar nav

--- components/sidebar/Sidebar.jsx
+++ components/sidebar/Sidebar.jsx
@@ -7,16 +7,24 @@ import { useQuery } from "react-query";
 import { useSession } from "@supabase/auth-helpers-react";
 
 export default function Sidebar(props) {
-  const [activeTab, setActiveTab] = useState("feed");
+  const [activeTab, setActiveTab] = useState("trade");
   const session = useSession();
 
   return (
     <>
-      <div className="flex justify-center space-x-2">
+      <div className="flex  space-x-2">
+        <a
+          onClick={() => setActiveTab("trade")}
+          className={` py-2 text-gray-400 hover:text-gray-700 rounded-lg cursor-pointer ${
+            activeTab === "trade" ? "text-gray-700 underline" : ""
+          }`}
+        >
+          Trade
+        </a>
         <a
           onClick={() => setActiveTab("feed")}
           className={`px-2 py-2 text-gray-400 hover:text-gray-700 rounded-lg cursor-pointer ${
-            activeTab === "feed" ? "text-gray-900 underline" : ""
+            activeTab === "feed" ? "text-gray-700 underline" : ""
           }`}
         >
           Feed
@@ -24,7 +32,7 @@ export default function Sidebar(props) {
         <a
           onClick={() => setActiveTab("leagues")}
           className={`px-2 py-2 text-gray-400 hover:text-gray-700 rounded-lg cursor-pointer ${
-            activeTab === "leagues" ? "text-gray-900 underline" : ""
+            activeTab === "leagues" ? "text-gray-700 underline" : ""
           }`}
         >
           League
@@ -37,14 +45,6 @@ export default function Sidebar(props) {
         >
           Team
         </a> */}
-        <a
-          onClick={() => setActiveTab("trade")}
-          className={`px-2 py-2 text-gray-400 hover:text-gray-700 rounded-lg cursor-pointer ${
-            activeTab === "trade" ? "text-gray-900 underline" : ""
-          }`}
-        >
-          Trade
-        </a>
       </div>
 
       {activeTab === "feed" && <NewFeedComponent />}
