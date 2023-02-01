d3a7f3b6a71e14f3813907f67b4b7986a85cbcec
Jason <jhughes3818@gmail.com>
2023-02-01 21:33:24 +1100

    add cursor pointer on sidebar tab

--- components/sidebar/Sidebar.jsx
+++ components/sidebar/Sidebar.jsx
@@ -90,7 +90,7 @@ export default function Sidebar(props) {
                     : "text-gray-500 hover:text-gray-700",
                   tabIdx === 0 ? "rounded-l-lg" : "",
                   tabIdx === tabs.length - 1 ? "rounded-r-lg" : "",
-                  "group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10"
+                  "group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10 cursor-pointer"
                 )}
                 aria-current={tab.current ? "page" : undefined}
               >
