5c401ab3ba3c663ed57671c9cd772f19095baf83
Jason <jhughes3818@gmail.com>
2023-02-13 16:40:47 +1100

    add fix for inactive drivers on teambuild

--- pages/rules.jsx
+++ pages/rules.jsx
@@ -1,21 +1,31 @@
 export default function Rules() {
   return (
-    <div className="grid place-items-center h-screen">
-      <div className="w-80">
-        <h1 className="text-2xl">Hello 👋</h1>
-        <h2 className="pb-4">Let&apos;s go racing.</h2>
+    <div className="grid place-items-center">
+      <div className="w-1/2">
+        <h1 className="text-3xl font-bold">Rules</h1>
+        <h2 className="text-xl font-bold">Building Your Team</h2>
         <p className="text-gray-600">
-          We&apos;ll start by building a team.
-          <br />
-          <br />
-          You have $15m to spend on drivers.
-          <br />
-          <br />
-          Each race, your drivers will earn points. The price of each driver
-          will also go up or down based on their performance.
-          <br />
-          <br />
-          You can build your budget by selling drivers for a profit!
+          Each player starts each season with $15m to spend on their team. A
+          team consists of 5 drivers.
+          <br></br>
+          During the season, drivers prices will go up and down depending on
+          their performance. The better a driver performs, the more expensive
+          they become.
+          <br></br>A player can grow their budget by buying and selling drivers
+          for a profit. If done correctly, a player will progressively be able
+          to afford more expensive, and thus better performing drivers.
+          <br></br> The value of a team is the sum of the values of its drivers
+          plus the remaining cash.
+        </p>
+        <h2 className="text-xl font-bold">Scoring Points</h2>
+        <p className="text-gray-600">
+          Each driver earns points based on their performance during a race
+          weekend. Points are awarded based on the driver's qualifying position,
+          finishing position, and the number of positions gained or lost during
+          the race.
+          <br></br>
+          For each race weekend, a team is awarded points equal to the sum of
+          the points earned by its drivers.
         </p>
       </div>
     </div>
